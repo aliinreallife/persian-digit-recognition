d4eb669a7f663ad0a97783f28c7235eb2d54b03b
ali <aliinreallifee@gmail.com>
2024-08-12 10:06:46 +0330

    add handle cors
    i did it the bad way :) but it will do the work for me

--- webui/app.py
+++ webui/app.py
@@ -3,8 +3,10 @@ import re
 import base64
 from recognizer import recgonize
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 
 @app.route("/")
